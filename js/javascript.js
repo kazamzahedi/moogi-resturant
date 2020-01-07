c2fa45485c3ed6e8b316f63354bd8a189a25bd4c
kazamzahedi <kazam.zahedi@gmail.com>
2020-01-07 15:18:26 +0100

    Fix responsive for MOBILE TABLET PC

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var x = document.getElementById("myLinks");
+  var x = document.getElementById('header__menu');
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
